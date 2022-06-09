ad3118de048d93a0b2c0e67e4fa6ab03a8fe7f5c
Robert Romano <bromano1992@gmail.com>
2022-06-09 17:56:21 -0400

    Created Products schema with validations added

--- mongoose_store/models/products.js
+++ mongoose_store/models/products.js
@@ -1,20 +1,32 @@
-//  //////////////////////////////////////////////
-// // Import Dependencies
-// //////////////////////////////////////////////
-// const mongoose = require("./connection");
+ //////////////////////////////////////////////
+// Import Dependencies
+//////////////////////////////////////////////
+const mongoose = require("./connection");
 
-// ////////////////////////////////////////////////
-// // Define Model
-// ////////////////////////////////////////////////
-// // pull schema and model from mongoose
-// const { Schema, model } = mongoose;
+////////////////////////////////////////////////
+// Define Model
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const { Schema, model } = mongoose;
 
-// // make PRODUCTS schema
-// const productsSchema = new Schema({
-//     name: String,
-//     description: String,
-//     img: String,
-//     price: Number,
-//     qty: Number
-// })
+// make PRODUCTS schema
+const productsSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    img: String,
+    price: {
+        type: Number,
+        min: 1
+    } ,
+    qty: {
+        type: Number,
+        min: 1,
+    }
+})
+
+// make product model
+const Product = model("Products", productsSchema);
 
